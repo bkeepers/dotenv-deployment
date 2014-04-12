cb2be08fc168d46b6ada7685e828646f18ad9399
Davydenkov Mihail <davydenkov_m@mail.ru>
2014-04-12 16:55:06 +0400

    ability-to-load-envs-from-config-path

--- lib/dotenv/deployment.rb
+++ lib/dotenv/deployment.rb
@@ -1,9 +1,11 @@
 require "dotenv"
 require "dotenv/deployment/version"
 
-# Load defaults
-Dotenv.load '.env'
+# Load defaults from .env or *.env in config
+configs = *Dir.glob("#{Rails.root}/config/**/*.env") if defined?(Rails)
+Dotenv.load '.env', configs
 
 # Override any existing variables if an environment-specific file exists
 environment = ENV['RACK_ENV'] || (defined?(Rails) && Rails.env)
-Dotenv.overload ".env.#{environment}" if environment
+configs.map! {|e| e + ".#{environment}"} if configs
+Dotenv.overload ".env.#{environment}", configs if environment
