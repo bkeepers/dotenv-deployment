5bdaf0a7f754621d473c19d5396c6a3a7bca4d9e
DavydenkovM <davydenkov_m@mail.ru>
2014-04-16 17:36:52 +0400

    Update deployment.rb

--- lib/dotenv/deployment.rb
+++ lib/dotenv/deployment.rb
@@ -2,13 +2,10 @@ require "dotenv"
 require "dotenv/deployment/version"
 
 # Load defaults from .env or *.env in config
-configs = []
-configs = *Dir.glob("#{Rails.root}/config/**/*.env") if defined?(Rails)
 Dotenv.load '.env', *configs
+Dotenv.load *Dir.glob("#{Rails.root}/config/**/*.env") if defined?(Rails)
 
 # Override any existing variables if an environment-specific file exists
 environment = ENV['RACK_ENV'] || (defined?(Rails) && Rails.env)
-configs.map! {|e| e + ".#{environment}"} 
-
 Dotenv.overload ".env.#{environment}"
 Dotenv.overload *Dir.glob("#{Rails.root}/config/**/*.env.#{environment}") if defined?(Rails)
