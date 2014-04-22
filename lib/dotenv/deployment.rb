27725d3240b045ff8e8a55244f0b02f521876a4a
Sijawusz Pur Rahnama <sija@sija.pl>
2014-04-22 14:31:34 +0200

    Remove reference to non-existant array.
    Fix for NameError: undefined local variable or method `configs' for main:Object

--- lib/dotenv/deployment.rb
+++ lib/dotenv/deployment.rb
@@ -2,7 +2,7 @@ require "dotenv"
 require "dotenv/deployment/version"
 
 # Load defaults from .env or *.env in config
-Dotenv.load '.env', *configs
+Dotenv.load '.env'
 Dotenv.load *Dir.glob("#{Rails.root}/config/**/*.env") if defined?(Rails)
 
 # Override any existing variables if an environment-specific file exists
