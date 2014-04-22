fa046c597a6e84ea068830ed238923c5257d68d0
Sijawusz Pur Rahnama <sija@sija.pl>
2014-04-22 14:19:11 +0200

    Wrong path for Capistrano recipes require

--- lib/dotenv/deployment/capistrano.rb
+++ lib/dotenv/deployment/capistrano.rb
@@ -3,7 +3,7 @@ require 'capistrano/version'
 if defined?(Capistrano::VERSION) && Capistrano::VERSION >= '3.0'
   raise 'Please read https://github.com/bkeepers/dotenv-deployment#capistrano to update your dotenv configuration for new Capistrano version'
 else
-  require 'dotenv/capistrano/recipes'
+  require 'dotenv/deployment/capistrano/recipes'
 
   Capistrano::Configuration.instance(:must_exist).load do
     before "deploy:finalize_update", "dotenv:symlink"
