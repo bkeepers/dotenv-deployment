22f4e6b57f2cc7c958bb1e2c98abe0ef5c7d3f4d
Oscar Rendon <orendon@gmail.com>
2014-05-13 16:05:35 -0500

    Remove defined?(Rails) checks
    Since dotenv-deployment was extracted as a separate gem from dotenv, then defined?(Rails) checks are not needed anymore.

--- lib/dotenv/deployment.rb
+++ lib/dotenv/deployment.rb
@@ -3,10 +3,10 @@ require "dotenv/deployment/version"
 
 # Load defaults from .env or *.env in config
 Dotenv.load '.env'
-Dotenv.load *Dir.glob("#{Rails.root}/config/**/*.env") if defined?(Rails)
+Dotenv.load *Dir.glob("#{Rails.root}/config/**/*.env")
 
 # Override any existing variables if an environment-specific file exists
-if environment = ENV['RACK_ENV'] || (defined?(Rails) && Rails.env)
+if environment = ENV['RACK_ENV'] || Rails.env)
   Dotenv.overload ".env.#{environment}"
-  Dotenv.overload *Dir.glob("#{Rails.root}/config/**/*.env.#{environment}") if defined?(Rails)
+  Dotenv.overload *Dir.glob("#{Rails.root}/config/**/*.env.#{environment}")
 end
