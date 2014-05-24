d8f5bbf3c09b6171c26822d195995ecd856d245e
Nathanial L. McConnell <FluxAugur@gmail.com>
2014-05-24 13:04:34 -0400

    adding Rails checks

--- lib/dotenv/deployment.rb
+++ lib/dotenv/deployment.rb
@@ -6,16 +6,16 @@ require "dotenv/core-ext/dotenv"
 
 # Load defaults from .env or *.env in config
 Dotenv.load '.env'
-Dotenv.load *Dir.glob("#{Rails.root}/config/**/*.env")
+Dotenv.load *Dir.glob("#{Rails.root}/config/**/*.env") if defined?(Rails)
 
 Dotenv.overload ".env.yml"
-Dotenv.load *Dir.glob("#{Rails.root}/config/**/*.env.yml")
+Dotenv.load *Dir.glob("#{Rails.root}/config/**/*.env.yml") if defined?(Rails)
 
 # Override any existing variables if an environment-specific file exists
 if environment = ENV['RACK_ENV'] || (defined?(Rails) && Rails.env)
   Dotenv.overload ".env.#{environment}"
-  Dotenv.overload *Dir.glob("#{Rails.root}/config/**/*.env.#{environment}")
+  Dotenv.overload *Dir.glob("#{Rails.root}/config/**/*.env.#{environment}") if defined?(Rails)
 
   Dotenv.overload ".env.#{environment}.yml"
-  Dotenv.overload *Dir.glob("#{Rails.root}/config/**/*.env.#{environment}.yml")
+  Dotenv.overload *Dir.glob("#{Rails.root}/config/**/*.env.#{environment}.yml") if defined?(Rails)
 end
