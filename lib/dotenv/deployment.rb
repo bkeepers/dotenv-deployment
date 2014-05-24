7e9ed0b374eb3646b04ab557fe74bc0f2d4d8296
Nathanial L. McConnell <FluxAugur@gmail.com>
2014-05-24 10:33:25 -0400

    Added support for YAML
    Added overloads for *.env.yml and *.env.{environment}.yml

--- lib/dotenv/deployment.rb
+++ lib/dotenv/deployment.rb
@@ -8,8 +8,14 @@ require "dotenv/core-ext/dotenv"
 Dotenv.load '.env'
 Dotenv.load *Dir.glob("#{Rails.root}/config/**/*.env")
 
+Dotenv.overload ".env.yml"
+Dotenv.load *Dir.glob("#{Rails.root}/config/**/*.env.yml")
+
 # Override any existing variables if an environment-specific file exists
 if environment = ENV['RACK_ENV'] || (defined?(Rails) && Rails.env)
   Dotenv.overload ".env.#{environment}"
   Dotenv.overload *Dir.glob("#{Rails.root}/config/**/*.env.#{environment}")
+  
+  Dotenv.overload ".env.#{environment}.yml"
+  Dotenv.overload *Dir.glob("#{Rails.root{/config/**/*.env.#{environment}.yml")
 end
