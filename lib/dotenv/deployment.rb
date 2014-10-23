4a5cee30a2119dbaf38736e112a26a45b192837c
Vincenzo Acinapura <vin.aci@gmail.com>
2014-10-23 13:59:47 +0200

    Fixes a load issue with specific .env files in Rails apps

--- lib/dotenv/deployment.rb
+++ lib/dotenv/deployment.rb
@@ -1,12 +1,14 @@
 require "dotenv"
 require "dotenv/deployment/version"
 
+rails_root = File.absolute_path("./")
+
 # Load defaults from .env or *.env in config
 Dotenv.load('.env')
-Dotenv.load(*Dir.glob("#{Rails.root}/config/**/*.env")) if defined?(Rails)
+Dotenv.load(*Dir.glob("#{rails_root}/config/**/*.env")) if defined?(Rails)
 
 # Override any existing variables if an environment-specific file exists
 if environment = ENV['RACK_ENV'] || (defined?(Rails) && Rails.env)
   Dotenv.overload(".env.#{environment}")
-  Dotenv.overload(*Dir.glob("#{Rails.root}/config/**/*.env.#{environment}")) if defined?(Rails)
+  Dotenv.overload(*Dir.glob("#{rails_root}/config/**/*.env.#{environment}")) if defined?(Rails)
 end
