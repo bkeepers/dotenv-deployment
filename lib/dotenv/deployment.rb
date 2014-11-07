43c40c62666e68eb04f8cee1665ef560fb8701e2
Anton Bogdanovich <27bogdanovich@gmail.com>
2014-11-07 00:29:13 -0800

    Fixes #13 .env overrides .env.#{environment}

--- lib/dotenv/deployment.rb
+++ lib/dotenv/deployment.rb
@@ -8,5 +8,8 @@ Dotenv.load(*Dir.glob("#{Rails.root}/config/**/*.env")) if defined?(Rails)
 # Override any existing variables if an environment-specific file exists
 if environment = ENV['RACK_ENV'] || (defined?(Rails) && Rails.env)
   Dotenv.overload(".env.#{environment}")
-  Dotenv.overload(*Dir.glob("#{Rails.root}/config/**/*.env.#{environment}")) if defined?(Rails)
+  if defined?(Rails)
+    config_env = Dir.glob("#{Rails.root}/config/**/*.env.#{environment}")
+    Dotenv.overload(*config_env) if config_env.any?
+  end
 end
